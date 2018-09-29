913299410ba7c296414f4999ae2b24f35b506e77
radmilatomic <radmila.tomic.rt@gmail.com>
2018-09-29 16:38:45 +0200

    changed initial state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,32 +1,25 @@
 const initialState = {
-animal:{
-	name:"Dog",
-	latinName:"Canis lupus familiaris",
-	dos:["lorem1","lorem2","lorem3"],
-	donts:["lorem1","lorem2","lorem3"],
-	Kingdom:"lorem1",
-   Phylum: "facts[3]",
-   Class : "facts[4]",
-   Order : "facts[5]",
-   CommonName : "facts[6]",
-   ScientificName : "facts[7]",
-   Location :"facts[8]",
-   Diet :"facts[9]",
-   Size :"facts[10]",
-   NumberOfSpicies :"facts[11]",
-   AverageLifeSpan :"facts[12]",
-   ConservationStatus :"facts[13]",
-   Colour :"facts[14]",
-   SkinType : "facts[15]",
-   FavouriteFood :"facts[16]",
-   Habitat :"facts[17]",
-   AverageLitterSize : "facts[18]",
-   MainPrey : "facts[19]",
-   Predators : "facts[20]",
-   SpecialFeatures : "facts[21]"
+	animal:{
+		name: "Dog",
+        Kingdom: "Animalia",
+        latinName:"Canis lupus familiaris",
+        Phylum: "Chordata",
+        Class: "Mammalia",
+        Order: "Carnivora",
+        Family:"Canidae",
+        Genus: "Canis",
+        ScientificName: "Canis lupus familiaris",
+        Type: "Mammal",
+        Diet: "Carnivore",
+        Size: "50cm (20in)",
+        Weight:"30kg (65lbs)",
+        TopSpeed: "50km/h (31mph)",
+        Lifespan:"15 years",
+        Lifestyle: "Solitary",
+        dos: ["Pet", "Be nice and kind"],
+        donts: ["Feed with sweets"]
+    }
 
-},
-  
 
 }
 
