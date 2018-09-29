2fddb57eb59e2d04ad6722ce7b7828eaa26e051f
tamaraaa <tamara.milosavljevic89@gmail.com>
2018-09-29 14:43:37 +0200

    animal connected to store

--- src/components/animal/index.js
+++ src/components/animal/index.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
 import './style.css';
 import hasky from '../../images/husky-database.jpg'
-class Animal extends Component{
+import arrow from '../../images/left.png'
+import {Link} from 'react-router-dom'
+import {connect} from 'react-redux'
+
+const mapStateToProps = state => {
+    return {animal : state.animal}
+}
+class ConnectedAnimal extends Component{
 
     render(){
         return(
            <div className='container'>
-            <div class='flex'>
+           <Link to ='/'>
+           <img className="arrow" src={arrow}/>
+           </Link>
+            <div className='flex'>
              <img className='animal' src={hasky}></img>
-            <h1 className='h'>Dog(Canis lupus familiaris)</h1>
-            <button class="btn btn-primary btn-lg">Details</button>
+            <h1 className='h'>{this.props.animal.name}({this.props.animal.latinName})</h1>
+            <Link to ='/details'>
+            <button className="btn btn-primary btn-lg">Details</button>
+            </Link>
             </div>
-           <h3 className='do'>DO : </h3> <ul className='do'><li>Lorem ipsum dolor sit amet, integer dictum gravida primis commodo, lorem lectus sit ut ante.</li> <li>Lorem ipsum dolor sit amet, integer dictum gravida primis commodo, lorem lectus sit ut ante.</li><li>Lorem ipsum dolor sit amet, integer dictum gravida primis commodo, lorem lectus sit ut ante.</li></ul>
-           <h3 className='dont'>DON'T : </h3> <ul className='do'><li>Lorem ipsum dolor sit amet, integer dictum gravida primis commodo, lorem lectus sit ut ante.</li> <li>Lorem ipsum dolor sit amet, integer dictum gravida primis commodo, lorem lectus sit ut ante.</li><li>Lorem ipsum dolor sit amet, integer dictum gravida primis commodo, lorem lectus sit ut ante.</li></ul>
+           <h3 className='do'>DO : </h3> 
+           <ul className='do'>
+           {this.props.animal.dos.map(li => <li>{li}</li>)}
+           </ul>
+           <h3 className='dont'>DON'T : </h3> 
+           <ul className='do'>
+           {this.props.animal.donts.map(li => <li>{li}</li>)}
+           </ul>
            
             
       </div>
@@ -20,5 +38,5 @@ class Animal extends Component{
     }
 
 }
-
+const Animal = connect(mapStateToProps)(ConnectedAnimal)
 export default Animal
