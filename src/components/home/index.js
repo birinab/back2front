dd624660f25f9f889f0016626f3ec8466132b74c
radmilatomic <radmila.tomic.rt@gmail.com>
2018-09-29 12:41:59 +0200

    added images

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './style.css';
 import {Link } from "react-router-dom";
+import homepageImage from '../../images/homepage.jpg';
 class Home extends Component{
 
     render(){
@@ -8,7 +9,7 @@ class Home extends Component{
            <div id="home" className="container">
             <div>
             <div>Take a picture of your animal</div>
-            <img src="http://pluspng.com/img-png/mobile-in-hand-png-snapshot-800.png"/>
+            <img src={homepageImage}/>
             </div>
             <Link to="/animal" style={{ textDecoration: 'none' }}><button className="linkButton">Scan the animal</button></Link>
       </div>
