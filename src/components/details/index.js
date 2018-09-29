78e655c7ccf021b3f7708ad38d76fddaddf5321c
radmilatomic <radmila.tomic.rt@gmail.com>
2018-09-29 17:33:27 +0200

    added animal details

--- src/components/details/index.js
+++ src/components/details/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import './style.css'
 
 const mapStateToProps = state => {
     return {animal : state.animal}
@@ -9,8 +10,13 @@ class ConnectedDetails extends Component{
     
     render(){
         return(
-           <div className='container'>
-            <h1>{this.props.animal.name}({this.props.animal.latinName})</h1>
+           <div className='container' id="details-container">
+            <div>This animal is named {this.props.animal.name}</div>
+            <div>It's latin name is {this.props.animal.latinName}</div>
+            <div>It belongs to kingdom {this.props.animal.Kingdom} and family {this.props.animal.Family}</div>
+            <div>This animal is {this.props.animal.Diet}</div>
+            <div>It weighs about {this.props.animal.Weight}</div>
+            <div>Approximate lifespan is {this.props.animal.Lifespan}</div>
             <div></div>
       </div>
       )
